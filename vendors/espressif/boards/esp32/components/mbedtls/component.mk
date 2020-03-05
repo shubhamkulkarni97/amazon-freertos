53478419f88a22fe617b173af8de514ddc1dcb05
Shubham Kulkarni <shubham.kulkarni@espressif.com>
2020-03-05 12:23:05 +0530

    Fix mbedtls when using LwIP stack

--- vendors/espressif/boards/esp32/components/mbedtls/component.mk
+++ vendors/espressif/boards/esp32/components/mbedtls/component.mk
@@ -5,5 +5,9 @@ COMPONENT_SRCDIRS := $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/library port \
 
 COMPONENT_ADD_INCLUDEDIRS := port/include $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/include
 
+ifdef AFR_ESP_LWIP
+COMPONENT_OBJEXCLUDE := ${AMAZON_FREERTOS_LIB_DIR}/3rdparty/mbedtls/library/net_sockets.o
+endif
+
 libraries/3rdparty/mbedtls/library/ecp.o: CFLAGS += -Wno-maybe-uninitialized
 libraries/3rdparty/mbedtls/library/ssl_tls.o: CFLAGS += -Wno-maybe-uninitialized
